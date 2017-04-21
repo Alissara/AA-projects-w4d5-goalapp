1ce1ac2d6be42fdce6df4bbbcb39d20bd75cd51b
alissara <alissara.r@gmail.com>
2017-04-21 14:08:49 -0700

    Add User controller and integration tests, start e-to-e tests

--- GoalApp/spec/controllers/users_controller_spec.rb
+++ GoalApp/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  # status codes
+  # responses
+  # valid / invalid
+
+  describe 'GET #index' do
+    it 'renders all users' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
+  describe 'GET #new' do
+    it "renders the new page" do
+      get :new
+      expect(response).to be_success
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST #create' do
+    context "with invalid params" do
+      it "renders the new page again" do
+        pass_params = { user: { username: 'tom', password: 'cat' } }
+        post :create, params: pass_params
+        expect(response).to render_template('new')
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it 'redirects to the users page' do
+        pass_params = { user: { username: 'bob', password: 'password' } }
+        post :create, params: pass_params
+        expect(response).to redirect_to(user_url(User.last))
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    subject(:user) { FactoryGirl.create(:user) }
+
+    context 'with valid user id' do
+      it 'renders the user page' do
+        get :show, params: {id: user.id}
+        expect(response).to be_success
+        expect(response).to render_template("show")
+      end
+    end
+
+    context 'with invalid user id' do
+      it 'goes back to the user index' do
+        get :show, params: {id: -1}
+        expect(response).to redirect_to(users_url)
+      end
+    end
+  end
 
 end
---
+++ GoalApp/spec/features/auth_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content('Sign Up')
+  end
+
+  feature "signing up a user" do
+    # fill out form
+    before(:each) do
+      visit new_user_url
+      fill_in 'username', with: 'bob'
+      fill_in 'password', with: 'password'
+      click_on 'Sign Up'
+    end
+
+    scenario "redirects to users page after signup" do
+      expect(page).to have_content("bob")
+    end
+
+    scenario "shows username on the homepage after signup" do
+      visit users_url
+      expect(page).to have_content("bob")
+    end
+  end
+end
+
+feature "logging in" do
+  scenario "shows username on the homepage after login" do
+
+  end
+end
+
+feature "logging out" do
+
+  scenario "begins with a logged out state"
+
+  scenario "doesn't show username on the homepage after logout"
+
+end
